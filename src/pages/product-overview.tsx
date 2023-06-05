1426479a08bca7d5ed79d4037e08be5302085de0
futjrnaut <futjrn@gmail.com>
2023-06-05 15:33:14 -0800

    ok last push

--- src/pages/product-overview.tsx
+++ src/pages/product-overview.tsx
@@ -90,6 +90,55 @@ const ProductOverview = () => {
           </div>
         </div>
         <hr />
+        <div >
+        <blockquote className={styles.quote}>
+          Made for Humans
+        </blockquote>
+        <div className={styles.paragraphContainer}>
+          <p className={styles.paragraph3}>
+            Our systems allow humans and AI <br /> to interact and discuss the best
+            route <br /> forward. Building the protocol for <br /> governance of these
+            discussions is our <br />mission.
+          </p>
+          </div>
+        </div>
+
+        <div className={styles.section}>
+  <div className={styles.sectionItem}>
+    <img src="/logotri.png" alt="Image 1" className={styles.image}/>
+    <Link href="/link/to/post1">Blog post 1</Link>
+  </div>
+  <div className={styles.sectionItem}>
+    <img src="/logotri.png" alt="Image 2" className={styles.image}/>
+    <Link href="/link/to/post2">Blog post 2</Link>
+  </div>
+  <div className={styles.sectionItem}>
+    <img src="/logotri.png" alt="Image 3" className={styles.image}/>
+    <Link  href="/link/to/post3">Blog post 3</Link>
+  </div>
+  <div className={styles.sectionItem}>
+    <img src="/logotri.png" alt="Image 4" className={styles.image}/>
+    <Link href="/link/to/post4">Blog post 4</Link>
+  </div>
+</div>
+<hr />
+<div className={styles.quoteSection}>
+  <blockquote className={styles.quote3}>
+    "Working at GenPen AI has brought <br /> me great fortune and wealth."
+  </blockquote>
+  <div className={styles.imageContainer}>
+    <img src="/logotri.png" alt="Career Image" className={styles.careerImage}/>
+  </div>
+</div>
+<hr />
+
+  <h1 className={styles.title}>Start creating with GenPen AI's <br /> powerful tools.</h1>
+  <div className={styles.buttons}>
+    <Link href="https://genpen.io/login.xhtml" className={styles.button}>Get Started</Link>
+    <Link href="/pricing" className={styles.button}>View pricing</Link>
+  </div>
+<hr />
+
           <Footer />
       </div>
   );

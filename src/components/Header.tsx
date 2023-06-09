15af07a23b679db5f5c7f31bbdf1ea67dd1fdb01
futjrnaut <futjrn@gmail.com>
2023-06-09 02:52:53 -0800

    work on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,46 +7,46 @@ import '../styles/globals.css';
 
 export const Header = () => (
   <header className={styles.header}>
-    <Link href="/">
+    <a href="/">
       <img src="/logo.png" alt="Logo" className={styles.logo} />
-    </Link>
+    </a>
     <nav className={styles.nav}>
       <ul className={styles.menu}>
         <li className={styles.menuItem}>
-          <Link href="/research-overview">Research</Link>
+          <a href="/research-overview">Research</a>
           <ul className={styles.submenu}>
-            <li><Link href="/research-overview">Overview</Link></li>
-            <li><Link href="/research-index">Index</Link></li>
+            <li><a href="/research-overview">Overview</a></li>
+            <li><a href="/research-index">Index</a></li>
           </ul>
         </li>
         <li className={styles.menuItem}>
-          <Link href="/product-overview">Product</Link>
+          <a href="/product-overview">Product</a>
           <ul className={styles.submenu}>
-            <li><Link href="/product-overview">Overview</Link></li>
-            <li><Link href="/scratchpad">ScratchPad</Link></li>
-            <li><Link href="/journal">Journal</Link></li>
-            <li><Link href="/ai1de">AI-IDE</Link></li>
-            <li><Link href="/holly">Holly</Link></li>
-            <li><Link href="/customer-stories">Customer Stories</Link></li>
-            <li><Link href="/pricing">Pricing</Link></li>
+            <li><a href="/product-overview">Overview</a></li>
+            <li><a href="/scratchpad">ScratchPad</a></li>
+            <li><a href="/journal">Journal</a></li>
+            <li><a href="/ai1de">AI-IDE</a></li>
+            <li><a href="/holly">Holly</a></li>
+            <li><a href="/customer-stories">Customer Stories</a></li>
+            <li><a href="/pricing">Pricing</a></li>
           </ul>
         </li>
         <li className={styles.menuItem}>
-          <Link href="/developer-overview">Developers</Link>
+          <a href="/developer-overview">Developers</a>
           <ul className={styles.submenu}>
-            <li><Link href="/developer-overview">Overview</Link></li>
-            <li><Link href="/developer-docs">Docs</Link></li>
-            <li><Link href="/developer-api">API Reference</Link></li>
-            <li><Link href="/developer-examples">Examples</Link></li>
+            <li><a href="/developer-overview">Overview</a></li>
+            <li><a href="/developer-docs">Docs</a></li>
+            <li><a href="/developer-api">API Reference</a></li>
+            <li><a href="/developer-examples">Examples</a></li>
           </ul>
         </li>
         <li className={styles.menuItem}>
-          <Link href="/about">Company</Link>
+          <a href="/about">Company</a>
           <ul className={styles.submenu}>
-            <li><Link href="/about">About</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
-            <li><Link href="/careers">Careers</Link></li>
-            <li><Link href="/charter">Charter</Link></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/careers">Careers</a></li>
+            <li><a href="/charter">Charter</a></li>
           </ul>
         </li>
       </ul>

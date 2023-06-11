c9053312feafb18f9a001304724ca080f6ac9f01
futjrnaut <futjrn@gmail.com>
2023-06-11 05:11:27 -0800

    button styles on homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,25 +12,25 @@ export default function Home() {
       <div className={styles.page}>
       <h1 className={styles.title}>Introducing GenPen IDE for Remote Desktop</h1>
       <div className={styles.buttons}>
-        <Link href="https://genpen.io/login.xhtml" className={styles.button}>Begin<br/>Session
-        </Link>
-        <Link href="https://www.youtube.com/@genpenai"
+        <a href="https://genpen.io/login.xhtml" className={styles.button}>Begin<br/>Session
+        </a>
+        <a href="https://www.youtube.com/@genpenai"
           className={styles.button}>The Making of GenPen AI
-        </Link>
+        </a>
       </div>
       </div>
       <div className={styles.section}>
         <div className={styles.container}>
           <h2 className={styles.smallHeader}>Development Costs <br />Should be Black & <br /> White</h2>
-          <Link href="./pricing"> Learn about our Pricing</Link>
+          <a href="./pricing" className={styles.button}> Learn about our Pricing</a>
         </div>
         <div className={styles.container}>
           <h2 className={styles.smallHeader}>Enterprise Scale <br /> REST ful Auto- <br />Genesis</h2>
-          <Link href="./research-overview">Learn about our Research</Link>
+          <a href="./research-overview" className={styles.button}>Learn about our Research</a>
         </div>
         <div className={styles.container}>
           <h2 className={styles.smallHeader}>Beginning the Basis <br /> for AI Polling with <br />Human Input</h2>
-          <Link href="./research-index">Learn about our Patents</Link>
+          <a href="./research-index" className={styles.button}>Learn about our Patents</a>
         </div>
         
       </div>
@@ -39,19 +39,19 @@ export default function Home() {
       <div className={styles.section}>
   <div className={styles.sectionItem}>
     <img src="/logotri.png" alt="Image 1" className={styles.image}/>
-    <Link href="/link/to/post1">Blog post 1</Link>
+    <a href="/link/to/post1" className={styles.button}>Blog post 1</a>
   </div>
   <div className={styles.sectionItem}>
     <img src="/logotri.png" alt="Image 2" className={styles.image}/>
-    <Link href="/link/to/post2">Blog post 2</Link>
+    <a href="/link/to/post2" className={styles.button}>Blog post 2</a>
   </div>
   <div className={styles.sectionItem}>
     <img src="/logotri.png" alt="Image 3" className={styles.image}/>
-    <Link  href="/link/to/post3">Blog post 3</Link>
+    <a  href="/link/to/post3" className={styles.button}>Blog post 3</a>
   </div>
   <div className={styles.sectionItem}>
     <img src="/logotri.png" alt="Image 4" className={styles.image}/>
-    <Link href="/link/to/post4">Blog post 4</Link>
+    <a href="/link/to/post4" className={styles.button}>Blog post 4</a>
   </div>
 </div>
 <hr />
@@ -61,29 +61,29 @@ export default function Home() {
     <img src="/logotri.png" alt="Image 1" className={styles.image}/>
     <h3>Title</h3>
     <h3>Price</h3>
-    <Link href="/link/to/demo">Demo</Link> <br />
-    <Link href="/link/to/purchase">Purchase</Link>
+    <a href="/link/to/demo" className={styles.button}>Demo</a> <br />
+    <a href="/link/to/purchase" className={styles.button}>Purchase</a>
   </div>
   <div className={styles.sectionItem}>
     <img src="/logotri.png" alt="Image 2" className={styles.image}/>
     <h3>Title</h3>
     <h3>Price</h3>
-    <Link href="/link/to/demo">Demo</Link> <br />
-    <Link href="/link/to/purchase">Purchase</Link>
+    <a href="/link/to/demo" className={styles.button}>Demo</a> <br />
+    <a href="/link/to/purchase" className={styles.button}>Purchase</a>
   </div>
   <div className={styles.sectionItem}>
     <img src="/logotri.png" alt="Image 3" className={styles.image}/>
     <h3>Title</h3>
     <h3>Price</h3>
-    <Link href="/link/to/demo">Demo</Link> <br />
-    <Link href="/link/to/purchase">Purchase</Link>
+    <a href="/link/to/demo" className={styles.button}>Demo</a> <br />
+    <a href="/link/to/purchase" className={styles.button}>Purchase</a>
   </div>
   <div className={styles.sectionItem}>
     <img src="/logotri.png" alt="Image 4" className={styles.image}/>
     <h3>Title</h3>
     <h3>Price</h3>
-    <Link href="/link/to/demo">Demo</Link> <br />
-    <Link href="/link/to/purchase">Purchase</Link>
+    <a href="/link/to/demo" className={styles.button}>Demo</a> <br />
+    <a href="/link/to/purchase" className={styles.button}>Purchase</a>
   </div>
 </div>
 <hr />
@@ -101,7 +101,7 @@ export default function Home() {
 <hr />
  <h1 className={styles.title}>Join us to make a brighter future.</h1>
   <div className={styles.buttons}>
-    <Link href="./careers" className={styles.button}>View Opportunities</Link>
+    <a href="./careers" className={styles.button}>View Opportunities</a>
   </div>
 
 <Footer />

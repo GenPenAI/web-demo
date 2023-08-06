86bced7e031ba18a766bf7491101d06fa4a0eb49
futjrnaut <futjrn@gmail.com>
2023-08-06 10:24:41 -0800

    push

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,60 +1,76 @@
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './modules/Header.module.css';
 import '../styles/globals.css';
 
-export const Header = () => (
-  <header className={styles.header}>
-    <Link href="/home">
-      <a>
-        <Image src="/logo.png" alt="Logo" width={100} height={100} className={styles.logo} />
-      </a>
-    </Link>
-    <nav className={styles.nav}>
-      <ul className={styles.menu}>
+export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link href="/home">
+        
+          <Image src="/logo.png" alt="Logo" width={100} height={100} className={styles.logo} />
+        
+      </Link>
+
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
+
+      <nav className={`${styles.nav} ${menuOpen ? styles.active : ''}`}>
+        <ul className={styles.menu}>
         <li className={styles.menuItem}>
-          <Link href="/research-overview"><a className={styles.button}>Research</a></Link>
+          <Link href="/research-overview" className={styles.button}>Research</Link>
           <ul className={styles.submenu}>
-            <li><Link href="/research-overview"><a className={styles.button}>Overview</a></Link></li>
-            <li><Link href="/research-index"><a className={styles.button}>Index</a></Link></li>
+            <li><Link href="/research-overview" className={styles.button}>Overview</Link></li>
+            <li><Link href="/research-index" className={styles.button}>Index</Link></li>
           </ul>
         </li>
         <li className={styles.menuItem}>
-          <Link href="/product-overview"><a className={styles.button}>Product</a></Link>
+          <Link href="/product-overview" className={styles.button}>Product</Link>
           <ul className={styles.submenu}>
-            <li><Link href="/product-overview"><a className={styles.button}>Overview</a></Link></li>
-            <li><Link href="/scratchpad"><a className={styles.button}>ScratchPad</a></Link></li>
-            <li><Link href="/journal"><a className={styles.button}>Journal</a></Link></li>
-            <li><Link href="/aide"><a className={styles.button}>AIDE</a></Link></li>
-            <li><Link href="/holly"><a className={styles.button}>Holly</a></Link></li>
-            <li><Link href="/customer-stories"><a className={styles.button}>Customer Stories</a></Link></li>
-            <li><Link href="/pricing"><a className={styles.button}>Pricing</a></Link></li>
+            <li><Link href="/product-overview" className={styles.button}>Overview</Link></li>
+            <li><Link href="/scratchpad" className={styles.button}>ScratchPad</Link></li>
+            <li><Link href="/journal" className={styles.button}>Journal</Link></li>
+            <li><Link href="/aide" className={styles.button}>AIDE</Link></li>
+            <li><Link href="/holly"className={styles.button}>Holly</Link></li>
+            <li><Link href="/customer-stories"className={styles.button}>Customer Stories</Link></li>
+            <li><Link href="/pricing" className={styles.button}>Pricing</Link></li>
           </ul>
         </li>
         <li className={styles.menuItem}>
-          <Link href="/developer-overview"><a className={styles.button}>Developers</a></Link>
+          <Link href="/developer-overview" className={styles.button}>Developers</Link>
           <ul className={styles.submenu}>
-            <li><Link href="/developer-overview"><a className={styles.button}>Overview</a></Link></li>
-            <li><Link href="/developer-docs"><a className={styles.button}>Docs</a></Link></li>
-            <li><Link href="/developer-api"><a className={styles.button}>API Reference</a></Link></li>
-            <li><Link href="/developer-examples"><a className={styles.button}>Examples</a></Link></li>
+            <li><Link href="/developer-overview" className={styles.button}>Overview</Link></li>
+            <li><Link href="/developer-docs" className={styles.button}>Docs</Link></li>
+            <li><Link href="/developer-api" className={styles.button}>API Reference</Link></li>
+            <li><Link href="/developer-examples" className={styles.button}>Examples</Link></li>
           </ul>
         </li>
         <li className={styles.menuItem}>
-          <Link href="/about"><a className={styles.button}>Company</a></Link>
+          <Link href="/about" className={styles.button}>Company</Link>
           <ul className={styles.submenu}>
-            <li><Link href="/about"><a className={styles.button}>About</a></Link></li>
-            <li><Link href="/blog"><a className={styles.button}>Blog</a></Link></li>
-            <li><Link href="/careers"><a className={styles.button}>Careers</a></Link></li>
-            <li><Link href="/charter"><a className={styles.button}>Charter</a></Link></li>
+            <li><Link href="/about" className={styles.button}>About</Link></li>
+            <li><Link href="/blog" className={styles.button}>Blog</Link></li>
+            <li><Link href="/careers" className={styles.button}>Careers</Link></li>
+            <li><Link href="/charter" className={styles.button}>Charter</Link></li>
           </ul>
         </li>
       </ul>
     </nav>
  
     <div className={styles.buttons}>
-      <Link href="https://genpen.io/login.xhtml"><a className={styles.loginButton}>Login</a></Link>
-      <Link href="https://genpen.io/signup.xhtml"><a className={styles.signUpButton}>Sign Up</a></Link>
-    </div>
-  </header>
-);
+        <Link href="https://genpen.io/login.xhtml" className={styles.loginButton}>Login</Link>
+        <Link href="https://genpen.io/signup.xhtml" className={styles.signUpButton}>Sign Up</Link>
+      </div>
+    </header>
+  );
+};
